dd68524fd7055b740d4f48b5d3859ab208aefef6
CaydenDev <78617487+CaydenDev@users.noreply.github.com>
2024-09-15 00:37:23 +0200

    Help Command Aktualisierung

--- bot.py
+++ bot.py
@@ -21,6 +21,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+bot.remove_command('help')
 
 @bot.command(name='help')
 async def help_command(ctx):
